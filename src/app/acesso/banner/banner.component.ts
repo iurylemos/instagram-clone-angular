fee44e78a6b9ab8341e65c7f141e18698a99cdc4
Iury Lemos <iurylemos10@gmail.com>
2019-10-08 22:01:09 -0300

    Model imagens para o banner e alterando template

--- src/app/acesso/banner/banner.component.ts
+++ src/app/acesso/banner/banner.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
+import { Imagem } from './imagem.model';
+
 
 @Component({
   selector: 'app-banner',
@@ -13,18 +15,26 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
       state('visivel', style({
         opacity: 1
       })),
-      transition('escondido => visivel', animate('1s ease-in')),
-      transition('visivel => escondido', animate('1s ease-in'))  
+      transition('escondido <=> visivel', animate('1s ease-in'))
     ])
   ]
 })
 export class BannerComponent implements OnInit {
+  
 
   public estado: string = 'visivel'
+  public imagens: Imagem[] = [
+    { estado: 'escondido', url: '../../../assets/banner-acesso/imagens/img_1.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/imagens/img_2.png' },
+    { estado: 'visivel', url: '../../../assets/banner-acesso/imagens/img_3.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/imagens/img_4.png' },
+    { estado: 'escondido', url: '../../../assets/banner-acesso/imagens/img_5.png' }
+  ]
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.imagens)
   }
 
   public toogleEstadoAnimacao() : void {
